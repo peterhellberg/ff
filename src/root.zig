bdceec40b7d70ce1d2a6b9366ee42146bcfa9a81
Peter Hellberg <peter@c7.se>
2025-02-16 15:10:19 +0100

    src: Add Point.new

--- src/root.zig
+++ src/root.zig
@@ -19,6 +19,13 @@ pub const Point = struct {
     x: i32 = 0,
     y: i32 = 0,
 
+    pub fn new(x: i32, y: i32) Point {
+        return .{
+            .x = x,
+            .y = y,
+        };
+    }
+
     pub fn add(self: Point, other: Point) Point {
         return .{
             .x = self.x + other.x,
